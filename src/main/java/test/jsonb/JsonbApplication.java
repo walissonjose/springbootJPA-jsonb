2732c6900988a99d89d4945b84ba57acafc93566
Walisson Araujo <walisson.araujo@ctec.ufal.br>
2023-01-23 10:16:09 -0300

    adding most archtecture of project

--- src/main/java/test/jsonb/JsonbApplication.java
+++ src/main/java/test/jsonb/JsonbApplication.java
@@ -2,8 +2,10 @@ package test.jsonb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JsonbApplication {
 
 	public static void main(String[] args) {
